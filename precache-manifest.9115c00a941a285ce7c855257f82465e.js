7600ad833a04b1255f241de82fdf8f93e5bfab26
Victor Lin <victorhlin@gmail.com>
2020-02-18 19:52:39 -0800

    Updates

---
+++ precache-manifest.9115c00a941a285ce7c855257f82465e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "783ca4edbc7c59d0e2458e3b5557619d",
+    "url": "/wedding/index.html"
+  },
+  {
+    "revision": "c034092cef373cb63e5f",
+    "url": "/wedding/static/css/main.dcfeae92.chunk.css"
+  },
+  {
+    "revision": "992736d798ac4fbf04b8",
+    "url": "/wedding/static/js/2.49f84f25.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/wedding/static/js/2.49f84f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c034092cef373cb63e5f",
+    "url": "/wedding/static/js/main.eac46c03.chunk.js"
+  },
+  {
+    "revision": "e1647d47836dbe28e216",
+    "url": "/wedding/static/js/runtime-main.8aa4f426.js"
+  },
+  {
+    "revision": "9937c7d81d6d2bdb5a6733fd7a518b4d",
+    "url": "/wedding/static/media/logo.9937c7d8.jpeg"
+  }
+]);
